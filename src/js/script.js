2c8a147b09d2da66cbcdda4a3fb0412dbc08aff4
Yaciner <redjalay@gmail.com>
2019-01-17 17:28:18 +0100

    added a svg circle representing a button and make it functional with the arduino board

--- src/js/script.js
+++ src/js/script.js
@@ -19,16 +19,36 @@ process.__defineGetter__('stdin', () => {
   return process.__stdin;
 }); {};
 
-var freq = 20;
-
-board.on('ready', function() {
+let freq = 20;
+let buttonCircle = document.querySelector('.button-circle');
+buttonCircle.style.fill = '#B84545';
+board.on('ready', () => {
   document.getElementById('board-status').src = './assets/ready.png';
   let circles = document.querySelectorAll('.st0');
 
-  var sensor = new five.Sensor({
+
+  let sensor = new five.Sensor({
     pin: 'A0',
     freq: freq,
   })
+
+  let button = new five.Button(2);
+
+  button.on("hold", () => {
+    console.log( "Button held" );
+    buttonCircle.style.fill = '#46B766';
+  });
+
+  button.on("press", () => {
+    console.log( "Button pressed" );
+    buttonCircle.style.fill = '#46B766';
+  });
+
+  button.on("release", () => {
+    console.log( "Button released" );
+    buttonCircle.style.fill = '#B84545';
+  });
+
 
   sensor.on("change", function() {
     let selected = this.scaleTo(0,6);
