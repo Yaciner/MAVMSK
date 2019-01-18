a4657d85cc384aa5a8bd28e4d64049d3f38e2be3
Yaciner <redjalay@gmail.com>
2019-01-18 01:13:42 +0100

    added interaction with the potentiometer and the button to zoom into a selected artwork

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const util = require('util');
 const five = require('johnny-five');
 const board = new five.Board();
-
 const Readable = require('stream').Readable;
+const mediumZoom = require('medium-zoom');
+
+
 class MyStream extends Readable {
   constructor(opts) {
     super(opts);
@@ -21,15 +23,20 @@ process.__defineGetter__('stdin', () => {
 let freq = 20;
 let buttonCircle = document.querySelector('.button-circle');
 buttonCircle.style.fill = '#B84545';
+let selected;
+
 board.on('ready', () => {
   document.getElementById('board-status').src = './assets/ready.png';
+  let artwork = document.querySelector('.artwork');
   let circles = document.querySelectorAll('.st0');
+  let indicator = document.querySelector('.pot');
 
 
   let sensor = new five.Sensor({
     pin: 'A0',
     freq: freq,
   })
+
 
   let button = new five.Button(2);
 
@@ -41,23 +48,27 @@ board.on('ready', () => {
   button.on("press", () => {
     console.log("Button pressed");
     buttonCircle.style.fill = '#46B766';
+    console.log(artwork);
+    artwork.classList.add(`zoomed-${selected + 1}`);
+    indicator.style.opacity = 0;
   });
 
   button.on("release", () => {
     console.log("Button released");
     buttonCircle.style.fill = '#B84545';
+    artwork.classList.remove(`zoomed-${selected + 1}`);
+    indicator.style.opacity = 1;
   });
 
 
   sensor.on("change", function() {
-    let selected = this.scaleTo(0,6);
+    selected = this.scaleTo(0,6);
     circles.forEach(circle => {
-      circle.style.fill = 'white';
+      circle.style.fill = 'none';
     })
 
     if(circles[selected]) {
       circles[selected].style.fill = 'black';
     }
-    console.log(selected);
   });
 })
